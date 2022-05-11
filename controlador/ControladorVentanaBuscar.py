a98a3e53ca45efa1139802e6a1e0489565c3524c
rully <rullyromerol@gmail.com>
2022-05-11 12:03:55 -0500

    La ventana para buscar pacientes esta terminada. Falta mostrar los usuarios asociados al terapeuta.

--- controlador/ControladorVentanaBuscar.py
+++ controlador/ControladorVentanaBuscar.py
@@ -2,6 +2,7 @@
 from vista.VentanaBuscar import VentanaBuscar
 from controlador.ControladorCrearUsuario import ControladorCrearUsuario
 from modelo.CrudUsuario import CrudUsuario
+from PyQt5 import QtWidgets as qtw
 
 class ControladorVentanaBuscar:
     def __init__(self, idTerapeuta):
@@ -24,6 +25,19 @@ class ControladorVentanaBuscar:
     def getPatients(self):
         # Get the patients id from the database
         self.Consulta = CrudUsuario()
-        self.Consulta.consultarPacientesAsociados(self.idTerapeuta)
+        listaPacientes = self.Consulta.consultarPacientesAsociados(self.idTerapeuta)
+        print(listaPacientes)
+        j =0
+        for i in listaPacientes:
+            #insert a row in the table
+            self.vista.table.insertRow(j)
+            self.vista.table.setItem(j, 0, qtw.QTableWidgetItem(i[1]))
+            self.vista.table.setItem(j, 1, qtw.QTableWidgetItem(i[2]))
+            self.vista.table.setItem(j, 2, qtw.QTableWidgetItem(i[3]))
+            self.vista.table.setItem(j, 3, qtw.QTableWidgetItem(i[4]))
+            self.vista.table.setItem(j, 4, qtw.QTableWidgetItem(i[5]))
+            self.vista.table.setItem(j, 5, qtw.QTableWidgetItem(i[7]))
+            self.vista.table.setItem(j, 6, qtw.QTableWidgetItem(i[8]))
+            self.vista.table.setItem(j, 7, qtw.QTableWidgetItem(i[12]))
 
-
+            j = j + 1
