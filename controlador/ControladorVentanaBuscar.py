af047c5d6d45d893133b3af0f63ef9dbfb97d591
rully <rullyromerol@gmail.com>
2022-05-12 14:25:47 -0500

    Ya se pueden editar los perfiles de los pacientes y el terapeuta puede asociar los pacientes agregados y visualizarlos en la tabla.

--- controlador/ControladorVentanaBuscar.py
+++ controlador/ControladorVentanaBuscar.py
@@ -14,30 +14,54 @@ class ControladorVentanaBuscar:
     def clicks(self):
         self.vista.btn_add.clicked.connect(self.add)
         self.vista.btn_close.clicked.connect(self.vista.close)
-        #self.vista.btn_delete.clicked.connect(self.delete)
+        self.vista.btn_delete.clicked.connect(self.delete)
+        self.vista.btn_edit.clicked.connect(self.edit)
 
     def add(self):
-        self.crear = ControladorCrearUsuario(1)
+        self.crear = ControladorCrearUsuario(1, self.idTerapeuta)
 
-    #def delete(self):
+    def delete(self):
+        # Get the selected row
+        row = self.vista.table.currentRow()
+        # Get the patient id
+        idPatient = self.vista.table.item(row, 0).text()
+        print(idPatient)
+        # Delete the patient from the database
+        self.Consulta.eliminarPacientes(idPatient)
+        # Delete the patient from the table
+        self.vista.table.removeRow(row)
+        # Close the window
+        self.vista.close()
+
 
     # Load patients from the database and show them in the table
     def getPatients(self):
         # Get the patients id from the database
         self.Consulta = CrudUsuario()
         listaPacientes = self.Consulta.consultarPacientesAsociados(self.idTerapeuta)
-        print(listaPacientes)
         j =0
+
         for i in listaPacientes:
             #insert a row in the table
             self.vista.table.insertRow(j)
-            self.vista.table.setItem(j, 0, qtw.QTableWidgetItem(i[1]))
-            self.vista.table.setItem(j, 1, qtw.QTableWidgetItem(i[2]))
-            self.vista.table.setItem(j, 2, qtw.QTableWidgetItem(i[3]))
-            self.vista.table.setItem(j, 3, qtw.QTableWidgetItem(i[4]))
-            self.vista.table.setItem(j, 4, qtw.QTableWidgetItem(i[5]))
-            self.vista.table.setItem(j, 5, qtw.QTableWidgetItem(i[7]))
-            self.vista.table.setItem(j, 6, qtw.QTableWidgetItem(i[8]))
-            self.vista.table.setItem(j, 7, qtw.QTableWidgetItem(i[12]))
+            self.vista.table.setItem(j, 0, qtw.QTableWidgetItem(str(i[0])))
+            self.vista.table.setItem(j, 1, qtw.QTableWidgetItem(i[1]))
+            self.vista.table.setItem(j, 2, qtw.QTableWidgetItem(i[2]))
+            self.vista.table.setItem(j, 3, qtw.QTableWidgetItem(i[3]))
+            self.vista.table.setItem(j, 4, qtw.QTableWidgetItem(i[4]))
+            self.vista.table.setItem(j, 5, qtw.QTableWidgetItem(i[5]))
+            self.vista.table.setItem(j, 6, qtw.QTableWidgetItem(i[7]))
+            self.vista.table.setItem(j, 7, qtw.QTableWidgetItem(i[8]))
+            self.vista.table.setItem(j, 8, qtw.QTableWidgetItem(i[12]))
 
             j = j + 1
+
+    # edit the patient data
+    def edit(self):
+        # Get the selected row
+        row = self.vista.table.currentRow()
+        print(row)
+        # Get the patient id
+        idPatient = self.vista.table.item(row, 0).text()
+        # Open the edit window
+        self.vistaEditar = ControladorCrearUsuario(2, self.idTerapeuta, idPatient)
